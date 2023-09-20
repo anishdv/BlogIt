fb09dcccbcf38f4aff63fb6d690e1226ff92fd3b
Anish Devnani <anishdev12@gmail.com>
2023-09-20 10:36:31 +0530

    Updates

---
+++ precache-manifest.137e083b23fd866606606d395c51aaca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a4bbbbfc17ef9057626f9a301add278",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc57d1c7adaad0f30766",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "4202c2f35ccce1ccd421",
+    "url": "/static/css/main.7aa0a794.chunk.css"
+  },
+  {
+    "revision": "cc57d1c7adaad0f30766",
+    "url": "/static/js/2.d04754c2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d04754c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4202c2f35ccce1ccd421",
+    "url": "/static/js/main.a68b0ff7.chunk.js"
+  },
+  {
+    "revision": "35c6bc6620fd0e11f636",
+    "url": "/static/js/runtime-main.0ed6e50a.js"
+  }
+]);
